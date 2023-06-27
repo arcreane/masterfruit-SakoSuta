b9b5c616726062e4c4f369c7b676c840285ff5bf
Emilie <92803520+SakoSuta@users.noreply.github.com>
2023-06-27 03:34:18 +0200

    💀👌

--- app/src/main/java/com/example/fruit/GuessNum.java
+++ app/src/main/java/com/example/fruit/GuessNum.java
@@ -8,7 +8,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -20,19 +19,15 @@ public class GuessNum extends AppCompatActivity {
         EASY(1, 10),
         MEDIUM(1, 50),
         HARD(1, 100);
-
         private final int min;
         private final int max;
-
         Difficulty(int min, int max) {
             this.min = min;
             this.max = max;
         }
-
         public int getMin() {
             return min;
         }
-
         public int getMax() {
             return max;
         }
@@ -52,12 +47,10 @@ public class GuessNum extends AppCompatActivity {
     private int scores;
     private int targetNumber;
     private int remainingAttempts;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess_num);
-
         numberEditText = findViewById(R.id.numberEditText);
         buttonValidate = findViewById(R.id.buttonValidate);
         progressBar = findViewById(R.id.progressBar);
@@ -67,25 +60,21 @@ public class GuessNum extends AppCompatActivity {
         radioDifficulty = findViewById(R.id.radioDifficulty);
         buttonStart = findViewById(R.id.buttonStart);
         checkBoxDifficulty = findViewById(R.id.checkBoxDifficulty);
-
         attempts = new ArrayList<>();
         scores = 0;
-
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startGame();
             }
         });
-
         buttonValidate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateNumber();
+                validate();
             }
         });
     }
-
     private void startGame() {
         radioDifficulty.setVisibility(View.GONE);
         buttonStart.setVisibility(View.GONE);
@@ -96,24 +85,21 @@ public class GuessNum extends AppCompatActivity {
         result.setVisibility(View.VISIBLE);
         score.setVisibility(View.VISIBLE);
         checkBoxDifficulty.setVisibility(View.VISIBLE);
-
-        difficulty = getSelectedDifficulty();
-        targetNumber = generateRandomNumber(difficulty);
+        difficulty = DifficultySelected();
+        targetNumber = RandomNumber(difficulty);
         remainingAttempts = 10;
         attempts.clear();
         scores = 0;
-
         progressBar.setMax(10);
         progressBar.setProgress(0);
         attempt.setText("Attempts: 0/10");
         score.setText("Score: 0");
         result.setText("");
-
         numberEditText.setText("");
         numberEditText.requestFocus();
     }
 
-    private void validateNumber() {
+    private void validate() {
         String input = numberEditText.getText().toString();
         if (!input.isEmpty()) {
             int guessedNumber = Integer.parseInt(input);
@@ -152,12 +138,12 @@ public class GuessNum extends AppCompatActivity {
             numberEditText.setText("");
             numberEditText.requestFocus();
 
-            updateScore();
+            ScoreUpdate();
             updateDifficulty();
         }
     }
 
-    private Difficulty getSelectedDifficulty() {
+    private Difficulty DifficultySelected() {
         int selectedId = radioDifficulty.getCheckedRadioButtonId();
         if (selectedId == R.id.Easy) {
             return Difficulty.EASY;
@@ -168,12 +154,12 @@ public class GuessNum extends AppCompatActivity {
         }
     }
 
-    private int generateRandomNumber(Difficulty difficulty) {
+    private int RandomNumber(Difficulty difficulty) {
         Random random = new Random();
         return random.nextInt(difficulty.getMax() - difficulty.getMin() + 1) + difficulty.getMin();
     }
 
-    private void updateScore() {
+    private void ScoreUpdate() {
         score.setText("Score: " + scores);
     }
 
